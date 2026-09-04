0b3c490481b2b72676fa09c417ea2e28366c4b07
AnatoliyNepom <tnepom1@gmail.com>
2026-09-04 16:15:21 +0300

    Практикуемся в использовании API клиентов

--- clients/files/files_client.py
+++ clients/files/files_client.py
@@ -1,20 +1,56 @@
+from httpx import Response
+
+from typing import TypedDict
+
+from clients.private_http_builder import get_private_http_client, AuthenticationUserDict
 from clients.api_client import APIClient
 
-from httpx import Response
-from typing import TypedDict
+
+class File(TypedDict):
+    """
+    Описание структуры файла.
+    """
+    id: str
+    url: str
+    filename: str
+    directory: str
 
 
 class CreateFileRequestDict(TypedDict):
+    """
+    Описание структуры запроса на создание файла.
+    """
     filename: str
     directory: str
     upload_file: str
 
+class CreateFileResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания файла.
+    """
+    file: File
 
 class FilesClient(APIClient):
+    """
+    Клиент для работы с /api/v1/files
+    """
+
     def get_file_api(self, file_id: str) -> Response:
+        """
+        Метод получения файла.
+
+        :param file_id: Идентификатор файла.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
         return self.get(f"api/v1/files/{file_id}")
 
     def create_file_api(self, request: CreateFileRequestDict) -> Response:
+        """
+        Метод создания файла.
+
+        :param request: Словарь с filename, directory, upload_file.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
         return self.post(
             "api/v1/files",
             data=request,
@@ -22,4 +58,23 @@ class FilesClient(APIClient):
         )
 
     def delete_file_api(self, file_id: str) -> Response:
+        """
+        Метод удаления файла.
+
+        :param file_id: Идентификатор файла.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
         return self.delete(f"api/v1/files/{file_id}")
+
+    def create_file(self, request: CreateFileRequestDict) -> CreateFileResponseDict:
+        response = self.create_file_api(request)
+        return response.json()
+
+
+def get_files_client(user: AuthenticationUserDict) -> FilesClient:
+    """
+    Функция создаёт экземпляр FilesClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию FilesClient.
+    """
+    return FilesClient(client = get_private_http_client(user))
